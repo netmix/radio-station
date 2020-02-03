fafce9732ce7e9df41e7064e5291bbc37df061e9
Andrew DePaula <depaan@gmail.com>
2020-02-03 15:04:45 -0800

    stubbed in help page for Edit shows admin page

--- help/contextual-help-config.php
+++ help/contextual-help-config.php
@@ -7,7 +7,6 @@
  */
 
  function rs_contextual_help( $contextual_help, $screen_id, $screen ) {
-  $prefix = $screen->parent_base . '_page_';
 
   //Add a custom if statement stanza per screen you wish to include help on.
   //Make sure the screen id in the if condition matches the page where you want the included help to appear
@@ -15,7 +14,25 @@
   //you can create multiple tabs on the same help screen by duplicating the inner
   //if statement multiple times (with a unique help tab id for each one).
 
+
+  //uncomment line below and view log file to id a particular screen
+  error_log("Displaying screen ". print_r($screen->id,true)."\n", 3, "/tmp/my-errors.log"); //code to write a line to wp-content/debug.log (works)
+  if ('edit-show' == $screen->id ) {
+    //import feature documentation tab
+    $help_file = RADIO_STATION_DIR . '/help/edit-show.php';
+    if (file_exists($help_file)) {
+      $content = render_php($help_file);
+      $screen->add_help_tab(array(
+        'id'        =>  'rs-edit-show',
+        'title'     =>  __('Managing Shows'),  //tab name
+        'content'   =>  $content
+      ));
+    }
+  }//if ( $prefix . 'import-export-shows' == $screen->id ) {
+
+
   //contextual help for import/export screen
+  $prefix = $screen->parent_base . '_page_';
   if ( $prefix . 'import-export-shows' == $screen->id ) {
 
     //import feature documentation tab
