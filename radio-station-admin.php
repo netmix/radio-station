f4729e750179da18750e9251f2ce0f7737759780
Andrew DePaula <depaan@gmail.com>
2020-02-21 13:21:41 -0800

    installed semantic-ui to the project via composer. Enqueued semantic-min.css on admin pages

--- radio-station-admin.php
+++ radio-station-admin.php
@@ -55,6 +55,9 @@ function radio_station_enqueue_admin_scripts() {
 	$version = filemtime( $script['file'] );
 	$deps = array( 'jquery', 'jquery-ui-datepicker' );
 	wp_enqueue_script( 'radio-station-admin', $script['url'], $deps, $version, true );
+
+	// --- enqueue semantic/ui css
+	wp_enqueue_style( 'semantic-ui-style', 'vendor/semantic/ui/dist/semantic.min.css', array(), '2.4.1');
 }
 
 
